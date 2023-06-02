5aa9ccd688535e3898799a3fdff1ea3a89fc7b0c
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2023-06-02 17:00:25 +0000

    Add code to increment score and incorrect answers telly

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert ("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert (`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 
@@ -57,12 +59,14 @@ function calculateCorrectAnswer() {
 }
 
 
-function incrementScrore() {
-
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore; 
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore; 
 }
 
 function displayAdditionQuestion(operand1, operand2) {
