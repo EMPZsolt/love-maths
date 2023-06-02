40f10e8e998d3e93caae0b11f09fcadaa19b32ad
Zsolt Gulyas <zsolt.gulyas86@gmail.com>
2023-06-02 17:33:27 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert (`unkown game type: ${gameType}`);
         throw `unkown game type: ${gameType}. Aborting! `;
@@ -50,7 +52,9 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
-        return[operand1 + operand2, "addition"]
+        return[operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return[operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -73,13 +77,14 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
-
 }
 
 function displaySubractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
